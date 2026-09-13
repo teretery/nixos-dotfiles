8d87d7860657a12c052cf24a4675f73f8ba053f3
teretery <50054631+teretery@users.noreply.github.com>
2026-09-13 14:15:50 +0300

    Add modules/nvidia.nix

---
+++ modules/nvidia.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+}
